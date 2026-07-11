dc5e5b6c4661de522306f3f93ea118391d925d6c
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-07-11 14:06:45 +0530

    created a function to checkBookExists

--- lib/actions/book.actions.ts
+++ lib/actions/book.actions.ts
@@ -4,6 +4,27 @@ import { generateSlug, serializeData } from "../utils";
 import Book from "@/database/models/book.model";
 import BookSegment from "@/database/models/book-segment.models";
 
+export const checkBookExists = async (title: string) => {
+  try {
+    await connectToDatabase();
+    const slug = generateSlug(title);
+    const existingBook = await Book.findOne({slug}).lean();
+
+    if(existingBook){
+      return{
+        exists:true,
+        data: serializeData(existingBook)
+      }
+    }
+  } catch (e) {
+    console.error("Error checking book exists", e);
+    return {
+      exists: false,
+      error: e,
+    };
+  }
+};
+
 export const createBook = async (data: CreateBook) => {
   try {
     await connectToDatabase();
@@ -61,14 +82,14 @@ export const saveBookSegments = async (
 
     await BookSegment.insertMany(segmentsToInsert);
 
-    await Book.findByIdAndUpdate(bookId,{totalSegments:segments.length});
+    await Book.findByIdAndUpdate(bookId, { totalSegments: segments.length });
 
-    console.log('Book sements saved successfully.')
+    console.log("Book sements saved successfully.");
 
-    return{
-        success:true,
-        data:{segmentsCreated:segments.length}
-    }
+    return {
+      success: true,
+      data: { segmentsCreated: segments.length },
+    };
   } catch (e) {
     console.error("Error saving book segments", e);
 
